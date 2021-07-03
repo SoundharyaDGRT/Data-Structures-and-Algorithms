f7b1e1b609aad55da2ae9e1bc2811d24590586bd
SoundharyaDGRT <soundnext123@gmail.com>
2021-07-03 10:15:16 +0530

    Update min.cpp

--- Linked List/05 Min Element in Linked List/min.cpp
+++ Linked List/05 Min Element in Linked List/min.cpp
@@ -18,7 +18,7 @@ void create(int A[], int n)
     first->next = NULL;
     last = first;
 
-    for (i = 0; i < n; i++)
+    for (i = 1; i < n; i++)
     {
         t = new Node;
         t->data = A[i];
